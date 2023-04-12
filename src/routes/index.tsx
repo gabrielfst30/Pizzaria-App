277ab0560acd533c536da2453c0dcc0dc61cb5f5
gabrielfst <you@example.com>
2023-04-12 14:14:08 -0300

    feature: tela de carregando

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View } from "react-native/types";
+import { View, ActivityIndicator} from "react-native";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -8,6 +8,23 @@ import AuthRoutes from "./auth.routes";
 function Routes(){
     const isAuth = false;
     const loading = false;
+
+    if(loading){
+        return(
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: '#1D1D2E',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >    
+
+                <ActivityIndicator size={60} color="#FFF" />
+            </View>
+        )
+    }
+
 
 
     //Se tiver logado <AppRoutes/> se não <AuthRoutes/>
