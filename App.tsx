dd264367ff7ce69e6f2e50994e8f485b4dfc3aaa
gabrielfst <you@example.com>
2023-04-13 14:46:56 -0300

    feature: criação do auth provider

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 
+import { AuthProvider } from "./src/contexts/AuthContext";
+
 //importando o navigation container porque o navigation pede
 //Routes chama as rotas para ser mostrada na página
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" translucent={false}/>
-      <Routes />
+      <AuthProvider>
+          <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" translucent={false}/>
+          <Routes />
+      </AuthProvider>
     </NavigationContainer>
-     
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,106 @@
+import React, { useState, createContext, ReactNode } from "react";
+
+//SALVAR OS DADOS OFFLINE
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+//importando API
+import { api } from "../services/api"
+
+//Tipagem do Context
+type AuthContextData = { 
+    user: UserProps;
+    isAuthenticated: boolean;
+    signIn: (loginInfo: SignInProps) => Promise<void> //tipando a função de login
+}
+
+//Tipando dados do user
+type UserProps = {
+    id: string;
+    name: string;
+    email: string;
+    token: string;
+}
+
+//Children vai ajudar na renderização da página
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+//Tipando dados do login
+type SignInProps = {
+    email: string;
+    password: string;
+}
+
+//Criando context
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({children}: AuthProviderProps){
+    //o user seguirá o tipo UserProps
+    const [user, setUser] = useState<UserProps>({
+        id: '',
+        name: '',
+        email: '',
+        token: ''
+    })
+
+    //Loading de login
+    const [loadingAuth, setLoadingAuth] = useState(false) //falso - loading desligado
+
+    //convertendo o user para booleano com '!!', se o user estiver preenchido indica que o usuario esta logado
+    const isAuthenticated =  !!user.name;
+
+    //Logando user
+    async function signIn({ email, password }: SignInProps){
+        setLoadingAuth(true); //quando clicar no login o loading fica true
+
+        //Mandando login do usuário para API  !***********!
+        try{
+            const response = await api.post('/session',{
+                email,
+                password
+            });
+
+            //console.log(response.data)
+
+            //PEGANDO OS DADOS DO USUÁRIO
+            const {id, name, token} = response.data
+
+            //TRAZENDO O RESPONSE E TRANSFORMANDO EM UM OBJETO
+            const data = {
+               ...response.data
+            }
+
+            //SALVANDO NO ASYNC STORAGE ID, NAME E TOKEN PARA LOGIN
+            await AsyncStorage.setItem('@sujeitopizzaria', JSON.stringify(data))
+
+            //DEPOIS QUE LOGAR E TIVER O TOKEN, AUTORIZE TODOS OS OUTROS REQUESTS QUE NECESSITE DE LOGIN COM AUTENTICAÇÃO
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+            //PREENCHE A USE STATE USER COM OS DADOS DO USUÁRIO
+            setUser({
+                id,
+                name,
+                email,
+                token
+
+            })
+
+            setLoadingAuth(false)
+
+        //Caso der errado:
+        }catch(error){
+            console.log('Erro ao acessar', error)
+            setLoadingAuth(false); //se o login der erro o loading para de rodar
+        }
+
+    }
+
+
+    //todas nossa páginas serão renderizadas no provider e enviadas para routes
+    return(
+        <AuthContext.Provider value = {{ user, isAuthenticated, signIn }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
