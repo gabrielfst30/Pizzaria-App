24491e4a3e55a5bd142789a215a35e3dfeb841f3
gabrielfst <you@example.com>
2023-04-14 10:28:13 -0300

    feature: loading tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,14 +4,15 @@ import { View,
          StyleSheet,
          Image,
          TextInput,
-         TouchableOpacity } from 'react-native';
+         TouchableOpacity, 
+         ActivityIndicator } from 'react-native';
 
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function SignIn(){
 
     //CONSUMINDO AS INFORMAÇÕES PARA LOGIN DO USUÁRIO COM AUTHCONTEXT
-    const { signIn } = useContext(AuthContext)
+    const { signIn, loadingAuth } = useContext(AuthContext)
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -52,7 +53,13 @@ export default function SignIn(){
                 />
 
                 <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                    <Text style={styles.buttonText}>Acessar</Text>
+ 
+                    { loadingAuth ?(
+                        <ActivityIndicator size={25} color='#fff'/>
+                    ):(
+                        <Text style={styles.buttonText}>Acessar</Text>
+                    )}
+                    
                 </TouchableOpacity>
             </View>
         </View>
