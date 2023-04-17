c293fe0bd4b7bc21a903162b278cb205cc3f2081
gabrielfst <you@example.com>
2023-04-17 14:45:49 -0300

    feature: criação tela dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,85 @@
-import React, { useContext } from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from "react-native";
 
-import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigation } from "@react-navigation/native"; 
+
+//importando tipagem das rotas
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamsList } from "../../routes/app.routes";
 
 export default function Dashboard(){
-    const { signOut } = useContext(AuthContext)
+    //criando navigation
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
 
+    //recebe o numero da mesa
+    const [ table, setTable ] = useState('');
+
+    async function openOrder(){
+        if(table == ''){
+            return alert('Preencha o número da mesa!')
+        }
+
+        //faz a requisição de abrir mesa e navega para a proxima tela!.
+        navigation.navigate('Order', { number: table, order_id: ''})
+    }
 
     return(
-        <View>
-            <Text>Tela dashboard</Text>
-            <Button
-                title='Sair do app'
-                onPress={signOut}
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Novo pedido</Text>
+            
+            <TextInput
+                placeholder="Numero da mesa"
+                placeholderTextColor='#bdb7b7'
+                style={styles.input}
+                keyboardType="numeric"
+                value={table}
+                onChangeText={setTable}
             />
-        </View>
+
+            <TouchableOpacity style={styles.button} onPress={openOrder}>
+                <Text style={styles.buttonText}>Abrir mesa</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        backgroundColor: '#1d1d2e',
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 24,
+    },
+    input:{
+        width: '90%',
+        height: 60,
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#FFF',
+    },
+    button:{
+        width: '90%',
+        height: 40,
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        marginVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        fontSize: 18,
+        color: '#101026',
+        fontWeight: 'bold'
+    }
+})
