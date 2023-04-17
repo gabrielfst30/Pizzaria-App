e8ab7bbf55262c30f885302e96cd6e09a9995eb5
gabrielfst <you@example.com>
2023-04-17 14:46:16 -0300

    feature: rotas e types

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
+import Order from '../pages/Order';
 
-const Stack = createNativeStackNavigator();
+//Tipando as rotas para o typescript
+export type StackParamsList = {
+    Dashboard: undefined,
+    Order: {//detalhando a order
+        number: number | string;
+        order_id: string;
+    };
+}
+
+
+const Stack = createNativeStackNavigator<StackParamsList>();
 
 //Rotas privadas para users logados
 function AppRoutes(){
     return(
         <Stack.Navigator>
-            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen 
+            name="Dashboard" 
+            component={Dashboard} 
+            options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+             name="Order" 
+             component={Order} 
+             options={{ headerShown: false }}
+            />
         </Stack.Navigator>
+        
     )
 }
 
